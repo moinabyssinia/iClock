68bb54e20aaabd120df7977f3a461d1476a33575
moinabyssinia <michaelg.tadesse@gmail.com>
2020-07-03 23:47:53 -0400

    added hand animations

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 // alert("Hvala!")
-var handSec = document.getElementsByClassName("sec")
+
+//selecting the second hand
+
+var handSec = document.getElementsByClassName("sec")[0];
+var handmin = document.getElementsByClassName("min")[0];
+var handhr = document.getElementsByClassName("hr")[0];
+var x = 0;
+
+//sec animation
+setInterval(function(){
+    x+=6;
+    handSec.style.transform = "rotate("+x+"deg)";
+}, 1000);
+//min animation
+setInterval(function(){
+    x+=6;
+    handmin.style.transform = "rotate("+x+"deg)";
+}, 1000*60);
+//hr animation
+setInterval(function(){
+    x+=6;
+    handhr.style.transform = "rotate("+x+"deg)";
+}, 1000*3600);
